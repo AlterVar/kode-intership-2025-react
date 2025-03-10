d0d44107d8e020e9640f5a70d76207c185c57486
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2025-03-10 12:20:20 +0600

    refactor: add styled-components, remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-function App() {
+import GlobalStyles from "./components/styles/GlobalStyles";
 
+import Search from "./components/Search";
+
+function App() {
   return (
     <>
-      
+      <GlobalStyles />
+      <Search />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
