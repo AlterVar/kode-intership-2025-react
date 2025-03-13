3149731250ddc09d78a46848745a950d8a8df1cd
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2025-03-13 21:16:38 +0600

    feat: reload people from LoadingError component

--- src/components/Error/LoadingError.tsx
+++ src/components/Error/LoadingError.tsx
@@ -1,6 +1,10 @@
 import styled from "styled-components";
 
 import ufo from "../../assets/images/requestError_ufo.png";
+import { departments } from "../../types/RequestParamsType";
+import { fetchPeople } from "../../app/features/peopleSlice";
+import { useAppDispatch } from "../../app/hooks";
+import { MouseEvent } from "react";
 
 const Loading = styled.div`
   display: flex;
@@ -42,15 +46,24 @@ const Loading = styled.div`
 `;
 
 const LoadingError = () => {
+	const dispatch = useAppDispatch();
+
+	const reloadPeople = (e: MouseEvent) => {
+			e.preventDefault();
+			dispatch(fetchPeople({ __example: departments["Все"] }));
+		};
+
   return (
     <Loading>
-      <div className="image-container failed">
+      <div className="image-container">
         <img src={ufo} alt="UFO" />
       </div>
-      <div className="content failed">
+      <div className="content">
         <h2 className="title">Какой-то сверхразум все сломал</h2>
         <p className="description">Постараемся быстро починить</p>
-        <button className="try-again">Попробовать снова</button>
+        <button className="try-again" onClick={reloadPeople}>
+          Попробовать снова
+        </button>
       </div>
     </Loading>
   );
