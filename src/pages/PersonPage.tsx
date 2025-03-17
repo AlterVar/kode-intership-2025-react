bb74090d521111d9eb6261ab93a8855ca8bb04ed
Alexandra Kolpakova <alexandra.k.w@yandex.ru>
2025-03-17 14:31:11 +0600

    feat: load person page via link

--- src/pages/PersonPage.tsx
+++ src/pages/PersonPage.tsx
@@ -1,10 +1,12 @@
-import { JSX } from "react";
+import { JSX, useEffect } from "react";
 import { IoIosArrowBack } from "react-icons/io";
 import { FaRegStar } from "react-icons/fa6";
 import { LuPhone } from "react-icons/lu";
 import styled from "styled-components";
 import { Link, useParams } from "react-router";
-import { useAppSelector } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { fetchPeople } from "../app/features/peopleSlice";
+import { departments } from "../types/RequestParamsType";
 
 const Header = styled.header`
   padding: 24px 20px;
@@ -19,7 +21,7 @@ const Header = styled.header`
     align-self: flex-start;
     width: 24px;
     height: 24px;
-		
+
     svg {
       fill: #050510;
       width: 18px;
@@ -33,10 +35,10 @@ const Header = styled.header`
     justify-content: center;
     width: 104px;
     height: 104px;
-    border-radius: 50%;
     background-color: #f7f7f8;
 
     img {
+      border-radius: 50%;
       width: 100%;
       height: 100%;
     }
@@ -97,97 +99,134 @@ const Info = styled.div`
     text-decoration: none;
   }
 
-   .description {
+  .description {
     color: #050510;
   }
 `;
 
 const Age = styled.p`
-    margin-left: auto;
-    color: #97979b;
+  margin-left: auto;
+  color: #97979b;
+`;
+
+const Loading = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+	height: 100vh;
+
+  p {
+    font-family: "InterBold", sans-serif;
+    font-size: 2.6rem;
+		color: #6534ff;
+  }
 `;
 
 const PersonPage = (): JSX.Element => {
-	const params = useParams();
-	const id = params.id;
-	const peopleState = useAppSelector((state) => state.people);
-	const person = peopleState.people.filter(person => person.id === id)[0];
-	const birthday = new Date(person.birthday);
+  const params = useParams();
+  const id = params.id;
+  const peopleState = useAppSelector((state) => state.people);
+  const dispatch = useAppDispatch();
 
-	const getFormattedAge = () => {
-		const today = new Date();
-		let age = today.getFullYear() - birthday.getFullYear();
-		let result = age.toString();
+  useEffect(() => {
+		if (peopleState.people.length === 0) {
+			/* dispatch(fetchPeople({ __code: "500"})); */
+      dispatch(fetchPeople({ __example: departments["Все"] }));
+    }
+  }, [dispatch, peopleState.people.length]);
 
-		if (
-      today.getMonth() < birthday.getMonth() ||
-      (today.getMonth() === birthday.getMonth() &&
-        today.getDate() < birthday.getDate())
-    ) {
-      age--;
-		}
-		
-		if (age % 10 === 1) {
-			result = age + " год";
-		} else if (age % 10 < 5) {
-			result = age + " года";
-		} else {
-			result = age + " лет";
-		}
-			
-			return result;
-	}
+  if (peopleState.state === "idle") {
+    const person = peopleState.people.filter((person) => person.id === id)[0];
+    const birthday = new Date(person.birthday);
 
-	const formatBirthday = () => {
-		return birthday.toLocaleString("ru-RU", {
-			day: "numeric",
-			month: "long",
-			year: "numeric",
-		}).slice(0, -2);
-	}
+    const getFormattedAge = () => {
+      const today = new Date();
+      let age = today.getFullYear() - birthday.getFullYear();
+      let result = age.toString();
 
-	const formatPhone = () => {
-		const phone = person.phone;
-		//+7 (900) 900 90 09
-		return phone.slice(0, 2) + " (" + phone.slice(2, 5) + ") " + phone.slice(5, 8) + " " + phone.slice(8, 10) + " " + phone.slice(10);
-	}
+      if (
+        today.getMonth() < birthday.getMonth() ||
+        (today.getMonth() === birthday.getMonth() &&
+          today.getDate() < birthday.getDate())
+      ) {
+        age--;
+      }
 
-  return (
-    <section>
-      <Header>
-        <Link to="/">
-          <IoIosArrowBack />
-        </Link>
-        <div className="image-container">
-          <img src={person.avatarUrl} alt="avatar" />
-        </div>
-        <div className="content">
-          <h2 className="person-name">
-            {person.firstName + " " + person.lastName}
-            <span>{" " + person.userTag}</span>
-          </h2>
-          <p>{person.department}</p>
-        </div>
-      </Header>
-      <Main>
-        <Info>
-          <div>
-            <FaRegStar />
+      if (age % 10 === 1) {
+        result = age + " год";
+      } else if (age % 10 < 5) {
+        result = age + " года";
+      } else {
+        result = age + " лет";
+      }
+
+      return result;
+    };
+
+    const formatBirthday = () => {
+      return birthday
+        .toLocaleString("ru-RU", {
+          day: "numeric",
+          month: "long",
+          year: "numeric",
+        })
+        .slice(0, -2);
+    };
+
+    const formatPhone = () => {
+      const phone = person.phone;
+      //+7 (900) 900 90 09
+      return (
+        phone.slice(0, 2) +
+        " (" +
+        phone.slice(2, 5) +
+        ") " +
+        phone.slice(5, 8) +
+        " " +
+        phone.slice(8, 10) +
+        " " +
+        phone.slice(10)
+      );
+    };
+
+    return (
+      <section>
+        <Header>
+          <Link to="/">
+            <IoIosArrowBack />
+          </Link>
+          <div className="image-container">
+            <img src={person.avatarUrl} alt="avatar" />
           </div>
-          <p className="description">{formatBirthday()}</p>
-          <Age>{getFormattedAge()}</Age>
-        </Info>
-        <Info>
-          <div>
-            <LuPhone />
+          <div className="content">
+            <h2 className="person-name">
+              {person.firstName + " " + person.lastName}
+              <span>{" " + person.userTag}</span>
+            </h2>
+            <p>{person.department}</p>
           </div>
-          <a href={"tel:" + person.phone} className="description">
-            {formatPhone()}
-          </a>
-        </Info>
-      </Main>
-    </section>
-  );
+        </Header>
+        <Main>
+          <Info>
+            <div>
+              <FaRegStar />
+            </div>
+            <p className="description">{formatBirthday()}</p>
+            <Age>{getFormattedAge()}</Age>
+          </Info>
+          <Info>
+            <div>
+              <LuPhone />
+            </div>
+            <a href={"tel:" + person.phone} className="description">
+              {formatPhone()}
+            </a>
+          </Info>
+        </Main>
+      </section>
+    );
+	}
+	return <Loading><p>Loading...</p></Loading>;
 };
 
 export default PersonPage;
